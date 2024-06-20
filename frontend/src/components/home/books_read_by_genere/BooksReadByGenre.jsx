df4fafe3d4d62e4d951017bfc2e109d843b2cad8
Getaneh48 <getaneh11@gmail.com>
2024-06-21 00:53:30 +0300

    book recommendation component fixed to fetch content from the backend server and books by genere component also fixed to fetch content from the backend server

--- frontend/src/components/home/books_read_by_genere/BooksReadByGenre.jsx
+++ frontend/src/components/home/books_read_by_genere/BooksReadByGenre.jsx
@@ -1,19 +1,39 @@
 import './booksreadbygenre.scss';
 import bb_genre_icon from '../../../assets/images/books-by-genre.png';
-import PropTypes from 'prop-types';
 import acadamic_icon from '../../../assets/images/genre/academic-icon.png';
 import art_icon from '../../../assets/images/genre/art-icon.png';
-import business_icon from '../../../assets/images/genre/business-icon.png';
+import business_icon from '../../../assets/images/genre/Business-and-Career.png';
 import fiction_icon from '../../../assets/images/genre/fiction-icon.png';
+import lifestyle_icon from '../../../assets/images/genre/Lifestyle.png';
+import poletics_icon from '../../../assets/images/genre/Poletics-and-Law.png';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import religion_icon from '../../../assets/images/genre/religion.png';
+import enviroment_icon from '../../../assets/images/genre/Enviroment.png';
+import health_icon from '../../../assets/images/genre/Health-and-Fitness.png';
+import technology_icon from '../../../assets/images/genre/Technology.png';
+import biography_icon from '../../../assets/images/genre/Biography.png';
+import fiction_career_icon from '../../../assets/images/genre/Fiction-and-Literature.png';
 
 
 function CountBooksByGenre({genre, count}) {
+    
+
     const icons = {
-        'Art': art_icon,
-        'Business': business_icon,
-        'Fiction': fiction_icon,
-        'Academic': acadamic_icon,
+        'Lifestyle': lifestyle_icon,
+        'Politics & Laws': poletics_icon,
+        'Technology': technology_icon,
+        'Academic & Education': acadamic_icon,
+        'Personal Growth': '',
+        'Religion': religion_icon,
+        'Science & Research': fiction_icon,
+        'Enviroment': enviroment_icon,
+        'Biography': biography_icon,
+        'Health and Fitness': health_icon,
+        'Fiction & Litrature': fiction_career_icon,
+        'Business & Career': business_icon,
+        'Art': art_icon
     }
 
     return (
@@ -23,30 +43,34 @@ function CountBooksByGenre({genre, count}) {
                 <img src={icons[genre]} alt="" />
             </div>
             <Link to="/"><span className="label">{genre}</span></Link>
+           
         </div>
     )
 }
 
 export default function BooksReadByGenre() {
+    const [genres, setGenres] = useState(null);
 
-    const lists = [
-        {
-            'genre': 'Art',
-            'count': 0
-        },
-        {
-            'genre': 'Business',
-            'count': 2,
-        },
-        {
-            'genre': 'Fiction',
-            'count': 10,
-        },
-        {
-            'genre': 'Academic',
-            'count': 3
+    useEffect(() => {
+        const fetchGenres = async () => {
+            try {
+                const url = 'http://localhost:5001/api/v1/booksread/by_genres';
+                const response = await fetch(url);
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log(data);
+                    setGenres(data);
+                } else {
+                    console.log(response);
+                }
+            } catch (error) {
+                console.log(error);
+            }
+            
         }
-    ];
+        fetchGenres();
+    },[])
+
 
     return (
         <section className="books-read-by-genere-container">
@@ -56,11 +80,14 @@ export default function BooksReadByGenre() {
             </div>
             <div className="brbg_body">
                 {
-                    lists.map((list, index) => {
+                    genres ? (
+                    Object.keys(genres).map((key, index) => {
                         return (
-                            <CountBooksByGenre key={index} genre={list.genre} count={list.count} />
+                            <CountBooksByGenre key={index} genre={key} count={genres[key]} />
                         )
-                    })
+                    })) : (
+                        <h3> No Genres Found</h3>
+                    )
                 }
             </div>
         </section>
@@ -68,6 +95,6 @@ export default function BooksReadByGenre() {
 }
 
 CountBooksByGenre.propTypes = {
-    genre: PropTypes.string.isRequired,
-    count: PropTypes.number.isRequired,
-};
+    genre: PropTypes.string,
+    count: PropTypes.number,
+}
--- frontend/src/components/home/recommended_books/RecommendedBooks.jsx
+++ frontend/src/components/home/recommended_books/RecommendedBooks.jsx
@@ -1,104 +1,54 @@
 import './recommendedbooks.scss';
 import recommended_books_icon from '../../../assets/images/recommended_books.png'
 import RecommendedBook from './recommended_book/RecommendedBook';
+import { useEffect, useState } from 'react';
+import loading_icon from '../../../assets/images/loading.gif';
 
 export default function RecommendedBooks() {
-    const recommended_books = [
-        {
-            "id": "3030151913",
-            "title": "Moral Reasoning at Work",
-            "subtitle": "Rethinking Ethics in Organizations",
-            "authors": "Øyvind Kvalnes",
-            "image": "https://www.dbooks.org/img/books/3030151913s.jpg",
-            "url": "https://www.dbooks.org/moral-reasoning-at-work-3030151913/"
-        },
-        {
-            "id": "9813299150",
-            "title": "Manual of Digital Earth",
-            "subtitle": "",
-            "authors": "Huadong Guo, Michael F. Goodchild, Alessandro Annoni",
-            "image": "https://www.dbooks.org/img/books/9813299150s.jpg",
-            "url": "https://www.dbooks.org/manual-of-digital-earth-9813299150/"
-        },
-        {
-            "id": "1491948965",
-            "title": "The Art of SEO",
-            "subtitle": "Mastering Search Engine Optimization",
-            "authors": "Jessie Stricchiola, Stephan Spencer, Eric Enge",
-            "image": "https://www.dbooks.org/img/books/1491948965s.jpg",
-            "url": "https://www.dbooks.org/the-art-of-seo-1491948965/"
-        },
-        {
-            "id": "5668680406",
-            "title": "Advanced Professional Communication",
-            "subtitle": "A Principled Approach to Workplace Writing",
-            "authors": "Melissa Ashman, Arley Cruthers",
-            "image": "https://www.dbooks.org/img/books/5668680406s.jpg",
-            "url": "https://www.dbooks.org/advanced-professional-communication-5668680406/"
-        },
-        {
-            "id": "9463722904",
-            "title": "Data Visualization in Society",
-            "subtitle": "",
-            "authors": "Martin Engebretsen, Helen Kennedy",
-            "image": "https://www.dbooks.org/img/books/9463722904s.jpg",
-            "url": "https://www.dbooks.org/data-visualization-in-society-9463722904/"
-          },
-          {
-            "id": "1911307401",
-            "title": "Visualising Facebook",
-            "subtitle": "A Comparative Perspective",
-            "authors": "Daniel Miller, Jolynna Sinanan",
-            "image": "https://www.dbooks.org/img/books/1911307401s.jpg",
-            "url": "https://www.dbooks.org/visualising-facebook-1911307401/"
-          },
-          {
-            "id": "1912656396",
-            "title": "Cultural Crowdfunding",
-            "subtitle": "Platform Capitalism, Labour and Globalization",
-            "authors": "Vincent Rouzé",
-            "image": "https://www.dbooks.org/img/books/1912656396s.jpg",
-            "url": "https://www.dbooks.org/cultural-crowdfunding-1912656396/"
-          },
-          {
-            "id": "3319685880",
-            "title": "Political Social Work",
-            "subtitle": "Using Power to Create Social Change",
-            "authors": "Shannon R. Lane, Suzanne Pritzker",
-            "image": "https://www.dbooks.org/img/books/3319685880s.jpg",
-            "url": "https://www.dbooks.org/political-social-work-3319685880/"
-          },
-          {
-            "id": "3319925318",
-            "title": "Mediation in Collective Labor Conflicts",
-            "subtitle": "",
-            "authors": "Martin Euwema, Francisco Medina, Ana Belén García, Erica Romero Pender",
-            "image": "https://www.dbooks.org/img/books/3319925318s.jpg",
-            "url": "https://www.dbooks.org/mediation-in-collective-labor-conflicts-3319925318/"
-          },
-          {
-            "id": "1137506806",
-            "title": "New Frontiers in Social Innovation Research",
-            "subtitle": "",
-            "authors": "Alex Nicholls, Julie Simon, Madeleine Gabriel",
-            "image": "https://www.dbooks.org/img/books/1137506806s.jpg",
-            "url": "https://www.dbooks.org/new-frontiers-in-social-innovation-research-1137506806/"
-          },
+    const [recommended_books, setRecommendedBooks] = useState([]);
+    const [loading_recommendation, setLoadingRecommendation] = useState(false);
 
-    ]
+    useEffect(()=>{
+        const getRecommendation = async () => {
+            const url = 'http://localhost:5001/api/v1/books_recommended';
+            try {
+                setLoadingRecommendation(true)
+                const response = await fetch(url);
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log(data);
+                    setRecommendedBooks(data);
+                    setLoadingRecommendation(false);
+                }
+            } catch (error) {
+                console.log(error);
+                setLoadingRecommendation(false);
+            }
+            
+        }
+
+        getRecommendation();
+    },[])
+
     return (
         <section className="recommended_books_container">
             <div className="rbooks_header">
                     <img src={recommended_books_icon} alt="Handbook of Medical Herbs"/>
                     <div className="title">Recommended Books</div>
             </div>
+            <div className={`${loading_recommendation ? "active loading-progress" : "loading-progress"}`}><img src={loading_icon} alt="Loading recommended books in progress" /></div>
             <div className="rbooks_body">
                 {
-                    recommended_books.map((rbook, index) => {
-                        return (
-                            <RecommendedBook rbook={rbook} key={index} />
-                        )
-                    })
+                    recommended_books ? (
+                        recommended_books?.map((rbook, index) => {
+                            return (
+                                <RecommendedBook rbook={rbook} key={index} />
+                            )
+                        })
+                    ) : (
+                        <h3>No recommendation available</h3>
+                    )
+                    
                 }
                 
             </div>
