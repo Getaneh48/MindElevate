92523f6b7040a0f3b4756b0d305d1568646717c0
Getaneh48 <getaneh11@gmail.com>
2024-07-09 20:56:48 +0000

    removed shell files

--- run_api_test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# run an api unittest
-
-python3 -m pytest tests/test_api -v -s
--- start_api_server.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-MELV_MYSQL_USER=getcho MELV_MYSQL_PWD=b51443a2338 MELV_MYSQL_HOST=localhost MELV_MYSQL_DB=mind_elevate MELV_TYPE_STORAGE=db MELV_API_PORT=5001 python3 -m api.v1.app
