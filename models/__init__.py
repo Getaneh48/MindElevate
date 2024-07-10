77b991102d9ba40d505395113e12af4333d0c65b
Getaneh48 <getaneh11@gmail.com>
2024-07-10 06:58:37 +0000

    Enable environment variables using dotenv (improves configuration)

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,3 @@
-from os import environ, getenv
 from models.user import User
 from models.book import Book
 from models.book_reading import BookReading
@@ -9,9 +8,12 @@ from models.friend import Friend
 from models.friend_request import FriendRequest
 from models.badge import Badge
 from models.book_genre import BookGenre
+import os
+from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
 
-if environ['MELV_TYPE_STORAGE'] == 'db':
+if os.environ.get('MELV_TYPE_STORAGE') == 'db':
     # storage type is database storage
     from models.engine.db_storage import DBStorage
     storage = DBStorage()
