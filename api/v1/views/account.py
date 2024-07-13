18b167cc8bd3f76cf2469d18c618fd283e199104
Getaneh48 <getaneh11@gmail.com>
2024-07-13 11:49:43 +0000

    Implemented the api account.py to handle endpoints login and register

--- api/v1/views/account.py
+++ api/v1/views/account.py
@@ -8,24 +8,6 @@ from passlib.hash import bcrypt
 from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 import re
 
-def validate_login_data(data):
-    if 'username' not in data:
-        return False
-    if 'password' not in data:
-        return False
-    return True
-
-def validate_reg_data(data):
-    if 'username' not in data:
-        return False
-    if 'password' not in data:
-        return False
-    if 'first_name' not in data:
-        return False
-    if 'last_name' not in data:
-        return False
-
-    return True
 
 def check_password_complexity(password):
     """
@@ -47,6 +29,42 @@ def check_password_complexity(password):
         len([lowercase.group(), uppercase.group(), number.group(), special.group()]) >= min_types
     )
 
+def validate_login_data(data):
+    if 'username' not in data:
+        return False
+    if 'password' not in data:
+        return False
+    return True
+
+def validate_reg_data(data):
+    errors = []
+    if 'username' not in data:
+        errors.append("username required")
+    else:
+        if not re.match(r"^[a-zA-Z]+[_0-9]*[a-zA-Z]*$", data['username']):
+            errors.append("Invalid username")
+    if 'password' not in data:
+        errors.append("password required")
+    else:
+        if not check_password_complexity(data['password']):
+            errors.append("password does not meet complexity criteria")
+
+    if 'first_name' not in data:
+        errors.append('First name required')
+    else:
+        if len(data['first_name']) < 2:
+            errors.append('First name should be >= 2 character')
+        else:
+            if not re.match(r"^[a-zA-Z]+[ ]*[a-zA-Z]+$", data['first_name']):
+                errors.append('Invalid first name')
+    if 'last_name' not in data:
+        errors.append('Last name required')
+    else:
+        if not re.match(r"^[a-zA-Z]+[ ]*[a-zA-Z]+$", data['last_name']):
+                errors.append('Invalid last name')
+
+    return errors
+
 @app_views.route('/login', methods=['POST'], strict_slashes=False)
 def login():
     if request.method == 'POST':
@@ -54,28 +72,29 @@ def login():
         if validate_login_data(data):
             user = storage.get_user_by_name(data['username'])
             if user:
-                if user.password == bcrypt.hash(data['password']):
-                    access_token = create_access_token(identity=data['username'])
-                    return jsonify({'success': True, 'message': 'Login successfull',\
-                                    'access_token': access_token}), 200
-                return jsonify({'success': False, 'message': 'Invalid password or username'}), 200
-            return jsonify({'success': False, 'message': 'Invalid username or password'}), 200
+                try:
+                    if bcrypt.verify(data['password'], user.password) is True:
+                        access_token = create_access_token(identity=user.id)
+                        return jsonify({'success': True, 'message': 'Login successfull',\
+                                        'access_token': access_token}), 200
+                    else:
+                        return jsonify({'success': False, 'message': 'Invalid username or password'}), 400
+                except ValueError as ex:
+                    print(ex)
+            return jsonify({'success': False, 'message': 'Invalid username or password'}), 400
         return jsonify({'success': False, 'message': 'Bad Request'}), 400
 
 @app_views.route('/register', methods=['POST'], strict_slashes=False)
 def register():
     if request.method == 'POST':
         data = request.get_json()
-        if validate_reg_data(data) is True:
+        errors = validate_reg_data(data)
+        if len(errors) == 0:
             user = storage.get_user_by_name(data['username'])
             # check if the username provided is taken or not
             if user is not None:
                 return jsonify({'success': False, 'message': 'username already taken'}), 200
             
-            # check if the password meets the complexity criteria
-            if check_password_complexity(data['password']) is not True:
-                return jsonify({'success': False, 
-                                'message': 'password does not meet complexity criteria'}), 400
             user_info = {
                     'username': data['username'],
                     'password': bcrypt.hash(data['password']),
@@ -97,5 +116,5 @@ def register():
                 print(ex)
                 return jsonify({'success': False, 'message': 'Interval Server Error'}), 500
         else:
-            return jsonify({'success': False, 'message': 'Bad Request'}), 400
+            return jsonify({'success': False, 'message': 'validation error', 'errors': errors}), 400
             
